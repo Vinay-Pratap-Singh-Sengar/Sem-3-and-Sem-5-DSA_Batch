9407558807d3496988da7f9208be1ad978c9f65f
vinay pratap singh sengar <vinaypratapsinghsengar@vinays-MacBook-Air-2.local>
2026-09-23 09:56:20 +0530

    first commit done

--- first.cpp
+++ first.cpp
@@ -1,7 +1,34 @@
+// #include <iostream>
+
+// int main(){
+//     std::cout<<"hello world!...";
+//     std::cout<<"this is my next print statement";
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
 int main(){
-    cout<<"hello world!...";
+    int a; // declaration
+    a = 10; // initialization
+    // declaration with initialization
+    int c = 20;
+    int sum;
+    sum = a + c ;
+    cout<<sum<<endl;
+    cout<<"hello world!..."<<endl;
+    cout<<"this is my next print statement"<<endl;
     return 0;
 }
+
+// when compilation 
+
+// preprocessor --> extended code 
+
+// compiler ---> assembly code
+
+// assembler --> object code 
+
+// object --> executed
